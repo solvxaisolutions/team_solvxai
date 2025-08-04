4b597bd5e282e7b2fb16846f8b1a4b160328c8c0
Dhinu-2001 <dhinucphilip1022001@gmail.com>
2025-08-04 20:01:06 +0530

    latest change

---
+++ src/splineComponent/Bot.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Bot() {
+return (
+    <Spline scene="https://prod.spline.design/H0A0gTkI-BQmJTyv/scene.splinecode" />
+  );
+}
